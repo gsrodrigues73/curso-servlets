4b4df23094368b7a59fd1f0804447c0b19504b97
gsrodrigues73 <gsrodrigues@yahoo.com>
2019-05-28 22:10:13 -0300

    Código estático

--- src/br/com/alura/gerenciador/servlet/Banco.java
+++ src/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,6 +7,15 @@ public class Banco {
 	
 	private static List <Empresa> listaEmpresa = new ArrayList<>();
 	
+	static {
+		Empresa empresa1 = new Empresa();
+		Empresa empresa2 = new Empresa();
+		empresa1.setNome("Alura");
+		empresa2.setNome("Caelum");
+		listaEmpresa.add(empresa1);
+		listaEmpresa.add(empresa2);
+	}
+	
 	public void adiciona(Empresa empresa) {
 		listaEmpresa.add(empresa);
 		
