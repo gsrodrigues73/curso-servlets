39c5ad88501cbb370e38bb28decb102569c574f2
gsrodrigues73 <gsrodrigues@yahoo.com>
2019-06-05 00:24:39 -0300

    Utilizando a biblioteca FMT do JSTL

--- src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,6 +1,9 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,18 +18,31 @@ public class NovaEmpresaServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		Empresa empresa = new Empresa();
 		String nomeEmpresa = request.getParameter("nome");
+		String dataAberturaParam = request.getParameter("data");
+		
+		Date dataAbertura = null;
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); 
+			dataAbertura = sdf.parse(dataAberturaParam);
+		} catch (ParseException e) {
+			throw new ServletException(e);
+		}
+		
+		Empresa empresa = new Empresa();
 		empresa.setNome(nomeEmpresa);
+		empresa.setDataAbertura(dataAbertura);
 		
 		Banco banco = new Banco();
 		banco.adiciona(empresa);
 		
 		System.out.println("Cadastrando nova empresa " + nomeEmpresa);
 		
-		request.setAttribute("empresa", nomeEmpresa);
 		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
 		rd.forward(request, response);
+		
 		//response.encodeRedirectURL("novaEmpresaCriada.jsp");
 	}
 
