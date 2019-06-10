85b3370ba3f02a13bca11362a0d5315d0df6bba3
gsrodrigues73 <gsrodrigues@yahoo.com>
2019-06-10 00:03:50 -0300

    Conhecendo o WEB.XML

--- src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
+++ src/br/com/alura/gerenciador/servlet/OiMundoServlet.java
@@ -3,13 +3,12 @@ package br.com.alura.gerenciador.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // Configurar URL de chamada
-@WebServlet(urlPatterns="/oi")
+//@WebServlet(urlPatterns="/oi")
 public class OiMundoServlet extends HttpServlet {
 	
 	private static final long serialVersionUID = -4628190983452256105L;
