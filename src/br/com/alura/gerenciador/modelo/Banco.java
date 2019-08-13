9d8215814e883988b1624c98dd65833a527f839c
gsrodrigues73 <gsrodrigues@yahoo.com>
2019-08-13 06:06:43 -0300

    Autenticacao 1: Criacao do formulario de login

--- src/br/com/alura/gerenciador/modelo/Banco.java
+++ src/br/com/alura/gerenciador/modelo/Banco.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class Banco {
 	
 	private static List <Empresa> listaEmpresa = new ArrayList<>();
+	private static List <Usuario> listaUsuario = new ArrayList<>();
 	
 	static {
 		Empresa empresa1 = new Empresa();
@@ -16,6 +17,17 @@ public class Banco {
 		
 		listaEmpresa.add(empresa1);
 		listaEmpresa.add(empresa2);
+		
+		Usuario usuario1 = new Usuario();
+		Usuario usuario2 = new Usuario();
+		
+		usuario1.setLogin("beto");
+		usuario1.setSenha("1234");
+		usuario2.setLogin("luciano");
+		usuario2.setSenha("1234");
+		
+		listaUsuario.add(usuario1);
+		listaUsuario.add(usuario2);
 	}
 	
 	public void adiciona(Empresa empresa) {
